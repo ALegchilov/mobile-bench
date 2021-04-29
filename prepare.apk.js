8ecce8e3698f602cd70fa6ef4411d0b61012dff4
Alexandr Legtchilov <Alexandr.Legtchilov@itechart-group.com>
2021-04-29 14:44:02 +0300

    Implement APK downloading prior to testing

--- prepare.apk.js
+++ prepare.apk.js
@@ -5,14 +5,29 @@ const path = require('path');
 
 const DIR = 'app';
 const APP_NAME = 'app-staging.apk'
-const APK_GOOGLE_DRIVE_PATH = 'https://drive.google.com/u/0/uc?export=download&confirm=5aSY&id=1Lj8hQo8SNeoU_cpbXQ6Z65fljqTKpz-9'
+const GOOGLE_DRIVE_PATH = 'https://drive.google.com/u/0'
+const DOWNLOAD_APK_PATH = `${GOOGLE_DRIVE_PATH}/uc?id=1Lj8hQo8SNeoU_cpbXQ6Z65fljqTKpz-9&export=download`
+const apkFilePath = path.join(DIR, APP_NAME);
 
 async function downloadApk() {
-    const buffer = await axios.get(APK_GOOGLE_DRIVE_PATH);
-    return buffer;
+    const response = await axios.get(DOWNLOAD_APK_PATH);
+    const confirmationPath = response.data.match(/"(\/uc?.+?)"/)[1].replace(/&amp;/g, '&');
+    const cookies = response.headers["set-cookie"][0]
+    console.log('Downloading APK. Download may take up to 3 minutes')
+    console.log(GOOGLE_DRIVE_PATH + confirmationPath);
+    const {data} = await axios.request({
+        url: GOOGLE_DRIVE_PATH + confirmationPath,
+        method: "get",
+        responseType: 'arraybuffer',
+        headers: {
+            Cookie: cookies
+        }
+    });
+    return data;
 }
 
-fse.ensureDir(DIR);
-if (!fs.existsSync(path.join(DIR, APP_NAME))) {
-    downloadApk().then(apk => fs.writeFileSync(APP_NAME, apk));
-}
+fse.ensureDir(DIR).then(() => {
+    if (!fs.existsSync(apkFilePath)) {
+        downloadApk().then(apk => fs.writeFileSync(apkFilePath, apk));
+    }
+});
